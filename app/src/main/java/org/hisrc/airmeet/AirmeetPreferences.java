0e94fdc0db280345e8c51256d9d74a2e2c9d0bbf
hunter <hantek.f@gmail.com>
2016-03-05 00:56:54 +0100

    Hunters files

--- app/src/main/java/org/hisrc/airmeet/AirmeetPreferences.java
+++ app/src/main/java/org/hisrc/airmeet/AirmeetPreferences.java
@@ -1,8 +1,11 @@
 package org.hisrc.airmeet;
 
+/**
+ * Created by Alexey Valikov on 3/4/2016.
+ */
 public class AirmeetPreferences {
     public static final String TOKEN = "token";
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
-    public static final String SUBSCRIBED_TOPICS = "subscribedTopics";
+
 }
