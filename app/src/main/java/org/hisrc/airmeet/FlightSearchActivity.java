4bd47cd4ec5ba4929f34ab3c2c2d2c4fa3e4ecc0
highsource <aleksei.valikov@gmail.com>
2016-03-05 01:45:18 +0100

    Work in progress.

--- app/src/main/java/org/hisrc/airmeet/FlightSearchActivity.java
+++ app/src/main/java/org/hisrc/airmeet/FlightSearchActivity.java
@@ -1,5 +1,6 @@
 package org.hisrc.airmeet;
 
+import android.app.DatePickerDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,11 +10,13 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.DatePicker;
+import android.widget.DatePicker;;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -27,8 +30,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class FlightSearchActivity extends AppCompatActivity  {
+public class FlightSearchActivity extends AppCompatActivity implements OnClickListener {
 
 
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
@@ -38,23 +44,37 @@ public class FlightSearchActivity extends AppCompatActivity  {
     private boolean isReceiverRegistered;
 
     // UI references.
-    private EditText mFlightNumber;
-    private DatePicker mDepartureDate;
+    private EditText mFlightNumberView;
+    private EditText mDepartureDateView;
     private Button mSubscribeButton;
+
+    //Date references
+    private SimpleDateFormat dateFormatter;
+    private EditText fromDateEtxt;
+    private EditText toDateEtxt;
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_search);
-        mFlightNumber = (EditText) findViewById(R.id.edit_text_flight_number);
-        mDepartureDate = (DatePicker) findViewById(R.id.date_picker_departure_date);
+        mFlightNumberView = (EditText) findViewById(R.id.flightNumber_editText);
+        mDepartureDateView = (EditText) findViewById(R.id.departureDate_editText);
 
-        Button mSubscribeButton = (Button) findViewById(R.id.button_subscribe);
-        mSubscribeButton.setOnClickListener(new OnClickListener() {
+        dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.GERMANY);
+
+        findViewsById();
+
+        setDateTimeField();
+
+        Button mSearchButton = (Button) findViewById(R.id.search_button);
+        mSearchButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String flightNumber = mFlightNumber.getText().toString().toUpperCase();
-                final LocalDate departureDate = new LocalDate(mDepartureDate.getYear(), mDepartureDate.getMonth() + 1, mDepartureDate.getDayOfMonth());
+                final String flightNumber = mFlightNumberView.getText().toString().toUpperCase();
+                DatePicker datePicker = fromDatePickerDialog.getDatePicker();
+                final LocalDate departureDate = new LocalDate(datePicker.getYear(), datePicker.getMonth() + 1, datePicker.getDayOfMonth());
                 searchFlight(flightNumber, departureDate);
             }
         });
@@ -66,6 +86,11 @@ public class FlightSearchActivity extends AppCompatActivity  {
                         PreferenceManager.getDefaultSharedPreferences(context);
                 boolean sentToken = sharedPreferences
                         .getBoolean(AirmeetPreferences.SENT_TOKEN_TO_SERVER, false);
+                if (sentToken) {
+//                    mInformationTextView.setText(getString(R.string.gcm_send_message));
+                } else {
+//                    mInformationTextView.setText(getString(R.string.token_error_message));
+                }
             }
         };
 
@@ -78,6 +103,52 @@ public class FlightSearchActivity extends AppCompatActivity  {
             startService(intent);
         }
     }
+
+    private void findViewsById() {
+        fromDateEtxt = (EditText) findViewById(R.id.departureDate_editText);
+        fromDateEtxt.setInputType(InputType.TYPE_NULL);
+        fromDateEtxt.requestFocus();
+
+        toDateEtxt = (EditText) findViewById(R.id.editText5);
+        toDateEtxt.setInputType(InputType.TYPE_NULL);
+    }
+
+    private void setDateTimeField() {
+        fromDateEtxt.setOnClickListener(this);
+        toDateEtxt.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(this, new OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                fromDateEtxt.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        toDatePickerDialog = new DatePickerDialog(this, new OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                toDateEtxt.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+
+
+    public void onClick(View view) {
+        if(view == fromDateEtxt) {
+            fromDatePickerDialog.show();
+        } else if(view == toDateEtxt) {
+            toDatePickerDialog.show();
+        }
+    }
+
 
     private void searchFlight(final String flightNumber, final LocalDate departureDate) {
         final FlightId flightId = new FlightId(flightNumber, departureDate);
@@ -142,8 +213,6 @@ public class FlightSearchActivity extends AppCompatActivity  {
             isReceiverRegistered = true;
         }
     }
-
-
     /**
      * Check the device to make sure it has the Google Play Services APK. If
      * it doesn't, display a dialog that allows users to download the APK from
@@ -164,5 +233,4 @@ public class FlightSearchActivity extends AppCompatActivity  {
         }
         return true;
     }
-
 }
